e6e880d288ae196fd9e796a43a4d7782ae477ce9
Eugene Dunn <134093002+QSC-Eugene@users.noreply.github.com>
2024-09-06 14:43:41 -0400

    Update copy_plugin.cmd

--- copy_plugin.cmd
+++ copy_plugin.cmd
@@ -1,7 +1,9 @@
 :: %1 workspaceFolder             C:\qsys_plugins\plugin
 :: %2 workspaceFolderBasename     plugin
 
-if not exist "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2" mkdir "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2"
+FOR /F "tokens=*" %%g IN ('powershell "(new-object -COM Shell.Application).Namespace(0x05).Self.Path"') do (SET DocumentsFolder=%%g)
+
+if not exist "%DocumentsFolder%\QSC\Q-Sys Designer\Plugins\%~2" mkdir "%DocumentsFolder%\QSC\Q-Sys Designer\Plugins\%~2"
 
 :: This line writes/overwrites any existing file without locking it
 ::      This introduced errors where on a file changed notification, TYPE wasn't finished, and QSD would see a partial script and
@@ -11,4 +13,4 @@ if not exist "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2" mkdir "%use
 :: As a solution, this line was added to write/overwrite files and lock them so that QSD couldn't access them until it was finished.
 ::      QSD was then throwing an access error while the file was being written, so I added code in C# to catch access denied errors,
 ::      in favour of waiting for the next changed notification that allowed the file to be read (i.e. file copying finished)
-COPY /Y "%1\%2.qplug" "%userprofile%\Documents\QSC\Q-Sys Designer\Plugins\%2\%2.qplug"
+COPY /Y "%~1\%~2.qplug" "%DocumentsFolder%\QSC\Q-Sys Designer\Plugins\%~2\%~2.qplug"
